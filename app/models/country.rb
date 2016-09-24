9c45dfa9e54b51d740a4428199c7028089be0dba
Alan Jeferson <alan.jeferson11@gmail.com>
2016-09-24 20:20:29 -0300

    Scaffold for countries and states

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,7 @@
 class Country < ActiveRecord::Base
 
+  has_many :states, foreign_key: 'country_name'
+
   self.primary_key = 'name'
 
 end
--- app/models/state.rb
+++ app/models/state.rb
@@ -1,3 +1,5 @@
 class State < ActiveRecord::Base
-  belongs_to :country
+
+  belongs_to :country, foreign_key: 'country_name'
+
 end
