e749e28b196cbf07f2aaee76909039d284ebda4c
Alan Jeferson <alan.jeferson11@gmail.com>
2016-09-24 21:29:53 -0300

    CRUD for publishers

--- app/helpers/addresses_helper.rb
+++ app/helpers/addresses_helper.rb
@@ -1,2 +1,8 @@
 module AddressesHelper
+
+  def addresses_options_for_select
+    addresses = Address.all
+    addresses.map { |address| ["#{address.street.kind} #{address.street.name} - #{address.number}", address.id] }
+  end
+
 end
