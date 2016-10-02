21379705639e5d569eecfc43a340764e25ae11bd
Alan Jeferson <alan.jeferson11@gmail.com>
2016-10-02 00:02:39 -0300

    Delete rules for user and messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ActiveRecord::Base
   has_many :evaluations, dependent: :destroy, foreign_key: 'user_username'
   has_many :exchanges_a, dependent: :nullify, foreign_key: 'user_a_username', class_name: 'Exchange'
   has_many :exchanges_b, dependent: :nullify, foreign_key: 'user_b_username', class_name: 'Exchange'
+  has_many :messages_addressee, dependent: :destroy, foreign_key: 'addressee_username', class_name: 'Message'
+  has_many :messages_sender, dependent: :destroy, foreign_key: 'sender_username', class_name: 'Message'
 
   validates_presence_of :name, :email, :username, :password
 
